84c0d8091475477e016b2bdffe1facbb28051252
SarahNevin <eaa25sjn@students.eaaa.dk>
2025-10-09 12:09:20 +0200

    Effekt tilføjet ved klik på svar

--- js/questions-script.js
+++ js/questions-script.js
@@ -14,14 +14,19 @@ function skjulInfo(){
 
 function korrektSvar(){
     document.getElementById("top-ikon").innerHTML = "<img src='assets/checkmark.svg' alt='Fly Leg'>";
-    document.getElementById("dark-overlay").style.display = "flex";
+    const overlay = document.getElementById("dark-overlay");
+    overlay.style.display = "flex";
+    setTimeout(() => overlay.classList.add("vis"), 10); // fade-in
+    
     document.getElementsByClassName("korrekt-feedback")[0].style.display = "block";
     document.getElementsByClassName("korrekt-feedback")[1].style.display = "block";}
 
 function forkertSvar(){
     document.getElementById("top-ikon").innerHTML = "<img src='assets/xmark.svg' alt='X-symbol'>";
-    document.getElementById("dark-overlay").style.display = "flex";
+    const overlay = document.getElementById("dark-overlay");
+    overlay.style.display = "flex";
+    setTimeout(() => overlay.classList.add("vis"), 10); // fade-in
+   
     document.getElementsByClassName("forkert-feedback")[0].style.display = "block";
     document.getElementsByClassName("forkert-feedback")[1].style.display = "block";
 }
-
